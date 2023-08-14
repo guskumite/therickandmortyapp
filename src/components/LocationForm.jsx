781875c8f829aa76f33d08e54242bfc054cb1c78
guskumite <gmolanoruiz2000@gmail.com>
2023-08-13 23:25:58 -0500

    version 1.0.3

--- src/components/LocationForm.jsx
+++ src/components/LocationForm.jsx
@@ -17,7 +17,7 @@ const LocationForm = ({ handleSubmit, knownDimensions }) => {
       onSubmit={handleSubmit}
     >
       <Select
-        className="text-black bg-[#355E33] h-[3vh] w-[45vw] mb-2"
+        className="text-black bg-[#355E33] h-[3vh] w-[45vw] mb-4"
         options={locations}
         name={"newLocation"}
       />
